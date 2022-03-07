a4348c794cc49a8614086610543f03be6cddb9a5
sojeong2020 <macpark09@gmail.com>
2022-03-07 11:54:28 +0000

    change notification

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../styles/home.css';
+import { Alert} from 'react-bootstrap';
 
 const Home = () => {
 
     return (
         <div>
-            <div class="alert alert-warning text-center" role="alert">
-                <h4 class="alert-heading">Covid 19 Community Tasks</h4>
-                <p>Looking for the covid specific community tasks? </p>
-                <a href="https://training10.teamkinetic.co.uk/vk/admin/" class="alert-link">Open the app</a>
-            </div>
+
+         <Alert variant="warning" className='text-center'>
+         <Alert.Heading>Covid 19 Community Tasks</Alert.Heading>
+         <p>Looking for the covid specific community tasks? </p>
+         <a href="https://training10.teamkinetic.co.uk/vk/admin/" class="alert-link">Open the app</a>
+         </Alert>
+         
         </div>
+        
     );
 };
 
